e06e73dbd21b8465f967920ced89b22c59e50582
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-03-02 21:28:17 -0800

    thank you for help feedback

--- src/components/HelpSolveYourProblem/HelpSolveYourProblem.tsx
+++ src/components/HelpSolveYourProblem/HelpSolveYourProblem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Text,
   Button,
@@ -6,17 +7,39 @@ import {
 import "./HelpSolveYourProblem.css";
 
 export const HelpSolveYourProblem = () => {
+  const [response, setResponse] = useState<string | null>(null);
+
+  const handleResponse = (answer: "Yes" | "No"): void => {
+    setResponse(answer);
+  };
+
   return (
     <div className="help-container">
-      <Text size="small">Did this page help solve your problem?</Text>
-      <ButtonGroup alignment="end">
-        <Button size="small" variant="secondary">
-          Yes
-        </Button>
-        <Button size="small" variant="secondary">
-          No
-        </Button>
-      </ButtonGroup>
+      {!response ? (
+        <>
+          <Text size="small">Did this page help solve your problem?</Text>
+          <ButtonGroup alignment="end">
+            <Button
+              size="small"
+              variant="secondary"
+              onPress={() => handleResponse("Yes")}
+            >
+              Yes
+            </Button>
+            <Button
+              size="small"
+              variant="secondary"
+              onPress={() => handleResponse("No")}
+            >
+              No
+            </Button>
+          </ButtonGroup>
+        </>
+      ) : (
+        <div className="response-message">
+          <Text size="small">Thank you for your response.</Text>
+        </div>
+      )}
     </div>
   );
 };
