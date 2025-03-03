eaa561716cbb2546a42be46b01225d81177836cb
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-03-02 17:39:29 -0800

    adjust popular buttons for mobile

--- src/components/PopularPages/PopularPages.tsx
+++ src/components/PopularPages/PopularPages.tsx
@@ -4,13 +4,32 @@ import {
   Heading,
 } from "@bcgov/design-system-react-components";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import data from "../../data/errors.json";
 import "./PopularPages.css";
 
 export const PopularPages = () => {
   const navigate = useNavigate();
-
+  const [orientation, setOrientation] = useState<"horizontal" | "vertical">("horizontal");
   const popularPages = data["popular-pages"];
+
+  const handleResize = () => {
+    if (window.innerWidth <= 768) {
+      setOrientation("vertical");
+    } else {
+      setOrientation("horizontal");
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    
+    window.addEventListener("resize", handleResize);
+    
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleButtonClick = (path: string) => {
     navigate(`/${path}`);
@@ -22,7 +41,7 @@ export const PopularPages = () => {
       <ButtonGroup
         alignment="start"
         ariaLabel="Popular pages"
-        orientation="horizontal" // make vetical on mobile/tablet
+        orientation={orientation}
       >
         {popularPages.map((page, index) => (
           <Button
