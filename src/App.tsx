16f67347a362f69847d6c9176daa6c9751fd918e
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-03-02 18:11:59 -0800

    fix blue background for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,19 @@ export default function App() {
     <BrowserRouter>
       <div className="site-wrapper">
         <HeaderPage />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/error/:errorCode" element={<ErrorDetails />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/update-log" element={<UpdateLog />} />
-          <Route path="/troubleshooting" element={<Troubleshooting />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <div className="content-wrapper">
+          <div className="blue-background"></div>
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/error/:errorCode" element={<ErrorDetails />} />
+              <Route path="/feedback" element={<Feedback />} />
+              <Route path="/update-log" element={<UpdateLog />} />
+              <Route path="/troubleshooting" element={<Troubleshooting />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </div>
         <FooterPage />
       </div>
     </BrowserRouter>
