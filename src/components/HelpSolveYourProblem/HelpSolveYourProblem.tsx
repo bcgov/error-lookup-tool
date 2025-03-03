2ca8d27f9284d36b2ae8a179487036ea1886d73f
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-03-02 20:38:34 -0800

    put feedback buttons in button group

--- src/components/HelpSolveYourProblem/HelpSolveYourProblem.tsx
+++ src/components/HelpSolveYourProblem/HelpSolveYourProblem.tsx
@@ -1,10 +1,22 @@
-import { Text } from "@bcgov/design-system-react-components";
+import {
+  Text,
+  Button,
+  ButtonGroup,
+} from "@bcgov/design-system-react-components";
 import "./HelpSolveYourProblem.css";
 
 export const HelpSolveYourProblem = () => {
   return (
     <div className="help-container">
       <Text>Did this page help solve your problem?</Text>
+      <ButtonGroup alignment="end">
+        <Button size="small" variant="secondary">
+          Yes
+        </Button>
+        <Button size="small" variant="secondary">
+          No
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
